4a1f94f902b963d412eb1288ee3f079ac27143fb
Robert Savu <robert@kreditech.com>
2016-01-05 17:04:48 +0100

    fix cbtools install script and version maps

--- install.sh
+++ install.sh
@@ -2,12 +2,15 @@
 
 repo="https://github.com/couchbase/couchbase-cli.git"
 v2="2.1.1"
-v3="3.0.x"
+v3="3.1.3"
+v4="4.1.0"
 path="lib/cbtools/"
 
 #clear directories in case the script has already been run; otherwise git complains and fails to clone
 rm -rf "$(dirname "$0")/"$path$v2
 rm -rf "$(dirname "$0")/"$path$v3
+rm -rf "$(dirname "$0")/"$path$v4
 
 git clone "-b" $v2 $repo "$(dirname "$0")/"$path$v2
 git clone "-b" $v3 $repo "$(dirname "$0")/"$path$v3
+git clone "-b" $v4 $repo "$(dirname "$0")/"$path$v4
